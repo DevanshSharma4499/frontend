51afeae5913cc141313b508f8726066803432d6a
DevanshSharma4499 <devanshsharma76823@gmail.com>
2024-04-06 18:21:43 +0530

    sec newone

--- src/makeRequest.js
+++ src/makeRequest.js
@@ -20,6 +20,6 @@ export const fetchDataFromApi = async (url) => {
 export const makeRequest = axios.create({
   baseURL: 'http://localhost:1337/api',
   headers: {
-    Authorization: "bearer " + '377a7654d760d00cb30d0f96162cebfd8353a12472365d0911a77abc938bf2db9403af13f91fb949b340b83e0f91ad18a9a268c74b1874e354b1880cb153592f0c100bdede1e2f450751762f6c3cbc3a8bdf5c399a1aff9d8dd7ed85b34d2264df5807b56aa4830ad5ed31dc3f6df8a713304af2c8587d9af8d5bbd389dadef3',
+    Authorization: "bearer " + '83f7fed96d560ed14345ce0ebca6f8b2c41af9767f59b8b8d662ab5601fd1eac9b147eafc8050301d19bd855b0c5100c1740bde409d57cea93df03ed9fd893a5af09643333611bba0daced5476257e8d364390d1f695be1018ebe94e9187c2cc619ecaac491363648efb5e255a9fe1ca5f87e2bae3eec139bff2e1c4778e43d5',
   },
 });
